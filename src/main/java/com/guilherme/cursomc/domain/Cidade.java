e973a2e59fc36681d9737f0be0bd165e4fb50696
Guilherme A. Marabezzi Clerice <marabezzi@yahoo.com.br>
2022-08-05 15:58:38 -0300

    Estado e cidade

--- src/main/java/com/guilherme/cursomc/domain/Cidade.java
+++ src/main/java/com/guilherme/cursomc/domain/Cidade.java
@@ -1,33 +1,38 @@
 package com.guilherme.cursomc.domain;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
-public class Categoria implements Serializable {
+public class Cidade implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
-	@JsonManagedReference
-	@ManyToMany(mappedBy="categorias")
-	private List<Produto> produtos = new ArrayList<>();
 	
-	public Categoria() {
+	@ManyToOne
+	@JoinColumn(name = "estado_id")
+	private Estado estado;
+
+	public Cidade() {
 		super();
+	}
+
+	public Cidade(Integer id, String nome, Estado estado) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.estado = estado;
 	}
 
 	public Integer getId() {
@@ -46,20 +51,14 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
-	public Categoria(Integer id, String nome) {
-		super();
-		this.id = id;
-		this.nome = nome;
+	public Estado getEstado() {
+		return estado;
 	}
 
-	public List<Produto> getProdutos() {
-		return produtos;
+	public void setEstado(Estado estado) {
+		this.estado = estado;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
-	}
-	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -73,8 +72,11 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Cidade other = (Cidade) obj;
 		return Objects.equals(id, other.id);
 	}
+	
+	
+	
 
-	}
+}
--- src/main/java/com/guilherme/cursomc/domain/Estado.java
+++ src/main/java/com/guilherme/cursomc/domain/Estado.java
@@ -9,24 +9,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
-public class Categoria implements Serializable {
+public class Estado implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
-	@JsonManagedReference
-	@ManyToMany(mappedBy="categorias")
-	private List<Produto> produtos = new ArrayList<>();
-	
-	public Categoria() {
+	@OneToMany(mappedBy="estado")
+	private List<Cidade> cidades = new ArrayList<>();
+
+	public Estado() {
 		super();
 	}
 
@@ -45,21 +43,22 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 
-	public Categoria(Integer id, String nome) {
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+
+	public Estado(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
 	}
 
-	public List<Produto> getProdutos() {
-		return produtos;
-	}
-
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
-	}
-	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -73,8 +72,10 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Estado other = (Estado) obj;
 		return Objects.equals(id, other.id);
 	}
-
-	}
+	
+	
+	
+}
