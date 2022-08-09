e276ab0032948856fd72e255d03902f728959f88
Guilherme A. Marabezzi Clerice <marabezzi@yahoo.com.br>
2022-08-09 12:41:21 -0300

    Validacao sistematica com Bean Validadtion

--- src/main/java/com/guilherme/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/guilherme/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,27 @@ package com.guilherme.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.guilherme.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	
+	/* No Spring acima de 2.3 tem que importar no POW a dependência
+ 	<dependency>
+    	<groupId>org.springframework.boot</groupId>
+    	<artifactId>spring-boot-starter-validation</artifactId>
+	</dependency>
+	 * */
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
