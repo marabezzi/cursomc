b4ed63d48559641b5ebc7ced17542dc6e4d6f634
Guilherme A. Marabezzi Clerice <marabezzi@yahoo.com.br>
2022-08-18 14:14:29 -0300

    Email HTML

--- src/main/java/com/guilherme/cursomc/services/PedidoService.java
+++ src/main/java/com/guilherme/cursomc/services/PedidoService.java
@@ -65,7 +65,7 @@ public class PedidoService {
 			}
 			itemPedidoRepository.saveAll(obj.getItens());
 			System.out.println(obj);
-			emailService.sendOrderConfirmationEmail(obj);
+			emailService.sendOrderConfirmationHtmlEmail(obj);
 			return obj;
 		}
 		
