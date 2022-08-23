c9f33855138f11173c1e0baff3a79cc9cabfeceb
Guilherme A. Marabezzi Clerice <marabezzi@yahoo.com.br>
2022-08-23 16:47:12 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/guilherme/cursomc/CursomcApplication.java
+++ src/main/java/com/guilherme/cursomc/CursomcApplication.java
@@ -1,13 +1,18 @@
 package com.guilherme.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.guilherme.cursomc.services.S3Service;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
 	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -15,6 +20,7 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("us.png");
 	}
 	
 	
